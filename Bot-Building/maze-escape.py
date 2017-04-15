63dd669c64cf632a9fb870e1d9e9c6f07e366c22
Murillo <msgrubler@gmail.com>
2017-04-15 17:33:23 -0300

    Maze Escape started

---
+++ Bot-Building/maze-escape.py
@@ -0,0 +1,9 @@
+# Maze Escape is a popular 1 player game where a bot is trapped in a 
+# maze and is expected to find its way out. In this version of the 
+# game, 2 bots whose positions are not visible to each other are placed 
+# at random points in the maze. the first bot to find its way out of 
+# the maze wins the game.
+
+# Link: https://www.hackerrank.com/challenges/botclean
+# Developer: Murillo Grubler
+
