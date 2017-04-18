a5907944e5a7ddb90471ef748c6eb9ebedacb35b
Murillo <msgrubler@gmail.com>
2017-04-17 22:38:23 -0300

    maze-escape in progress

--- Bot-Building/maze-escape.py
+++ Bot-Building/maze-escape.py
@@ -4,6 +4,30 @@
 # at random points in the maze. the first bot to find its way out of 
 # the maze wins the game.
 
-# Link: https://www.hackerrank.com/challenges/botclean
+# Link: https://www.hackerrank.com/challenges/maze-escape
 # Developer: Murillo Grubler
 
+# import library
+import os
+import math
+
+# Set next action the bot
+def next_move(player, board):
+    print("Player: {}".format(player))
+    move = ''
+    # Start
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if i == 0 and j == 1 and board[i][j] == '-':
+                move = 'UP'
+            if i == 0 and j == 1 and board[i][j] == '#':
+                move = 'RIGHT'
+    
+    print(move)
+
+# Start application
+if __name__ == "__main__":
+    # Set data
+    player = int(input())
+    board = [[j for j in input().strip()] for i in range(3)]  
+    next_move(player, board)
