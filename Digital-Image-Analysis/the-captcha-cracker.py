526d542e8964eb5dca5bcc316e57c4642c015c5f
Murillo <msgrubler@gmail.com>
2017-04-23 16:22:21 -0300

    Model finalized - The captcha cracker

--- Digital-Image-Analysis/the-captcha-cracker.py
+++ Digital-Image-Analysis/the-captcha-cracker.py
@@ -4,31 +4,60 @@
 # texture, nature of the font, spacing of the font, morphological 
 # characteristics of the letters and numerals, etc. 
 
-# Link: https://www.hackerrank.com/challenges/digital-camera-day-or-night
+# Link: https://www.hackerrank.com/challenges/the-captcha-cracker
 # Developer: Murillo Grubler
 
 # Import libraries
+import os
+import sys
+import numpy as np
 from sklearn.linear_model import LogisticRegression
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
 # Define functions
-def readData(directory_name):
-    return []
+def getInput(directory, filename_input):
+    pixels = []
+    with open(directory + "/input/" + filename_input, "r") as f:
+        next(f)
+        for line in f:
+            for item in line.split(" "):
+                vals = [int(val) for val in item.split(",")]
+                pixels.append(sum(vals)/len(vals))
+    return pixels
 
-def trainData(data):
-    return []
+def getOutput(directory, filename_input):
+    label = "output" + str(filename_input[5:].split(".")[0]) + ".txt"
+    with open(directory + "/output/" + label, "r") as f:
+        return f.read().strip()
+
+def getData(directory_name):
+    inputs = []
+    labels = []
+    for root, dirs, files in os.walk(directory_name + "/input", topdown=False):
+        files = [f for f in files if f.endswith(".txt")]
+        for item in files:
+            filename = os.path.join(root, item)
+            labels.append(getOutput(directory_name, item))
+            inputs.append(getInput(directory_name, item))
+    
+    return np.array(inputs), np.array(labels)
+
+# Directory of the captcha
+directory = sys.path[0] + "/sampleCaptchas"
 
 # Read the captchas
-data = readData(directory)
-
-# Separate data for training
-train =  trainData(data)
+input_data, output_data = getData(directory)
 
 # Create the classifier model
 clf = LogisticRegression()
-clf.fit(train[0], train[1])
+clf.fit(input_data, output_data)
+
+print(clf.predict(input_data[24]))
+print(output_data[24])
 
 # Set new captcha
-captcha = input().split(" ")
+#captcha = input().split(" ")
 
 # Predict new captcha
-print(clf.predict(captcha))
+#print(clf.predict(captcha))
