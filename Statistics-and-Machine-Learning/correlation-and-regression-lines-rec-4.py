f1ddb5d84af833c6b35e0839d5b08ab4a7abfb8c
Murillo <msgrubler@gmail.com>
2017-05-06 14:24:10 -0300

    Correlation and Regression Lines - A Quick Recap #4

---
+++ Statistics-and-Machine-Learning/correlation-and-regression-lines-rec-4.py
@@ -0,0 +1,14 @@
+# The two regression lines of a bivariate distribution are: 
+# 4x – 5y + 33 = 0 (line of y on x) 20x – 9y – 107 = 0 (line of x on y). 
+# Estimate the value of x when y = 7. Compute the correct answer to 
+# one decimal place.
+
+# Link: https://www.hackerrank.com/challenges/correlation-and-regression-lines-4
+# Reference: https://stats.stackexchange.com/questions/22718/what-is-the-difference-between-linear-regression-on-y-with-x-and-x-with-y
+# Developer: Murillo Grubler
+
+y = 7
+val_x = (9 * y + 107) / 20
+print (round(val_x, 1))
+
+# Result: 8.5
