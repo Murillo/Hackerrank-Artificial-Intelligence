68d427d8c560fc8d3a4189a92e3bff09bebae583
Murillo <msgrubler@gmail.com>
2017-05-13 14:58:38 -0300

    Challenge basic-probability-puzzles-2 added

---
+++ Probability-Statistics-Foundations/basic-probability-puzzles-2.py
@@ -0,0 +1,26 @@
+# Objective 
+# In this challenge, we practice calculating probability.
+
+# Task
+# For a single toss of 2 fair (evenly-weighted) dice, find the 
+# probability that the values rolled by each die will be different 
+# and their sum is 6.
+
+# Reference: https://en.wikipedia.org/wiki/Probability
+# GitHub: https://github.com/Murillo
+# Developer: Murillo Grubler
+
+probability = 1/6
+values = 6
+result = 0
+
+# First dice
+for i in range(1, values + 1):
+    # Second dice
+    for j in range(1, values + 1):
+        # Verify if each die will be different and their sum is 6
+        if i != j and (i + j) == 6:
+            result += probability ** 2
+
+# Final probability found
+print (result)
