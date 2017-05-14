f96dc1ac677ea761229df45e62dc4de6e0c4e6ac
Murillo <msgrubler@gmail.com>
2017-05-14 16:50:45 -0300

    Day 2: Basic Probability Puzzles #1 - added

--- Probability-Statistics-Foundations/basic-probability-puzzles-1.py
+++ Probability-Statistics-Foundations/basic-probability-puzzles-1.py
@@ -2,11 +2,10 @@
 # In this challenge, we practice calculating probability.
 
 # Task
-# For a single toss of 2 fair (evenly-weighted) dice, find the 
-# probability that the values rolled by each die will be different 
-# and their sum is 6.
+# In a single toss of 2 fair (evenly-weighted) 6-sided dice, 
+# find the probability of that their sum will be at most 9.
 
-# Challenge: https://www.hackerrank.com/challenges/basic-probability-puzzles-2
+# Challenge: https://www.hackerrank.com/challenges/basic-probability-puzzles-1
 # Reference: https://en.wikipedia.org/wiki/Probability
 # GitHub: https://github.com/Murillo
 # Developer: Murillo Grubler
@@ -20,7 +19,7 @@ for i in range(1, values + 1):
     # Second dice
     for j in range(1, values + 1):
         # Verify if each die will be different and their sum is 6
-        if i != j and (i + j) == 6:
+        if (i + j) <= 9:
             result += probability ** 2
 
 # Final probability found
