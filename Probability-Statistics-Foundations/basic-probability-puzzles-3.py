f62635a6e5c123ec7b83c70d115b971b97950ea4
Murillo <msgrubler@gmail.com>
2017-05-14 16:56:14 -0300

    start -> Day 2: Basic Probability Puzzles #3

---
+++ Probability-Statistics-Foundations/basic-probability-puzzles-3.py
@@ -0,0 +1,14 @@
+# Objective 
+# In this challenge, we practice calculating probability.
+
+# Task
+# There are 3 urns: X, Y and Z.
+# Urn X contains 4 red balls and 3 black balls.
+# Urn Y contains 5 red balls and 4 black balls.
+# Urn Z contains 4 red balls and 4 black balls.
+# One ball is drawn from each urn. What is the probability that the 3 balls drawn consist of 2 red balls and 1 black ball?
+
+# Challenge: https://www.hackerrank.com/challenges/basic-probability-puzzles-3
+# Reference: https://en.wikipedia.org/wiki/Probability
+# GitHub: https://github.com/Murillo
+# Developer: Murillo Grubler
